b25dc2f07ea1b5182756c195abc83c080af6c24e
gusarov2k <motoface@yandex.ru>
2018-08-29 16:19:29 +0300

    add code in migration

--- db/migrate/20180829131258_create_items.rb
+++ db/migrate/20180829131258_create_items.rb
@@ -1,8 +1,12 @@
 class CreateItems < ActiveRecord::Migration
   def change
     create_table :items do |t|
-
-      t.timestamps null: false
+		t.string 	:name
+		t.text 		:description
+		t.float 	:price
+		t.float 	:weight
+		
+		t.timestamps null: false
     end
   end
 end
