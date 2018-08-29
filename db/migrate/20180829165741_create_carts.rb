4717a8761b79eaed559dd36eefa4bcf522fe90f9
gusarov2k <motoface@yandex.ru>
2018-08-29 20:03:06 +0300

    add code in migration carts

--- db/migrate/20180829165741_create_carts.rb
+++ db/migrate/20180829165741_create_carts.rb
@@ -1,8 +1,9 @@
 class CreateCarts < ActiveRecord::Migration
   def change
     create_table :carts do |t|
-
-      t.timestamps null: false
+    	t.string 	:login
+    	t.string 	:email
+		t.integer 	:user_id
     end
   end
 end
