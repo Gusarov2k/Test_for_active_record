084372b8781b70be93f41f692aab3666cab62221
gusarov2k <motoface@yandex.ru>
2018-08-31 16:39:59 +0300

    bundle exec rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180829165741) do
+ActiveRecord::Schema.define(version: 20180831133221) do
 
   create_table "carts", force: :cascade do |t|
     t.integer "user_id"
@@ -24,6 +24,10 @@ ActiveRecord::Schema.define(version: 20180829165741) do
     t.float    "weight"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
