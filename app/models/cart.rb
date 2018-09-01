eaf628889ec8fa3ce2317d2a91a41a9d474d782a
gusarov2k <motoface@yandex.ru>
2018-09-01 21:15:33 +0300

    add links to model Cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -2,7 +2,7 @@ class Cart < ActiveRecord::Base
 	belongs_to :user
 	has_and_belongs_to_many :items
 
-	has_many :positions
+	has_many :positions, as: :container
 	has_many :items, through: :positions
 
 # update quantity item if add + 1
