70884c443dd838f84491af9188f5116387c623f9
gusarov2k <motoface@yandex.ru>
2018-09-01 21:14:58 +0300

    add links to model Item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ActiveRecord::Base
 	has_and_belongs_to_many :carts
-	has_many :positions
-	has_many :carts, through: :positions
-	has_many :images, as: :imageable
+	has_many 	:positions
+	has_many 	:carts, through: :positions, source: :container,
+						source_type: "Cart"
+	has_many 	:orders, through: :positions, source: :container,
+						source_type: "Order"
+	has_many 	:images, as: :imageable
 end
